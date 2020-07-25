8766228a671df76460495c65a1fe5192c9337ff6
Alice Aekyung Huh <aliceaekyunglee@alices-imac.myfiosgateway.com>
2020-07-25 10:19:19 -0400

    [word game] Add boiler tempelate and links css and js

---
+++ app.js
@@ -0,0 +1,20 @@
+// image appears
+// blank appears
+// options appears
+
+//event listner 
+// choose option
+//check if it's right or not 
+//if it's right 
+// chosen option disappears
+// the letter appears on the blank 
+// the player gets the point 
+// go to the next page 
+
+//if it's wrong
+//turn switch 
+
+//switch turns
+
+// winning condition 
+// 
